050b19047135fc7d6403876e8eda490221fa1dc8
sunguoli <sunguoli1996@163.com>
2023-03-29 18:44:56 +0800

    添加配置文件

--- tests/test.py
+++ tests/test.py
@@ -1,6 +1,18 @@
 import requests
+import configparser
+import os
 
-url = "https://east.albion-online-data.com/api/v2/stats/history/T4_BAG?time-scale=1"
+# 获取config.ini的路径
+config_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'config', 'config.ini'))
+
+# 创建config对象并读取配置文件
+config = configparser.ConfigParser()
+config.read(config_path)
+
+# 获取配置参数
+api_url = config.get('dataConfig', 'api_url')
+
+url = api_url + "v2/stats/history/T5_BAG@1?time-scale=1"
 
 response = requests.get(url)
 
